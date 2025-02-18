73d08e64f01a0990fc1d2640c8eb9749d8e66c09
Norka <xieyr1093@gmail.com>
2025-02-18 16:43:11 +0800

    navbar & home : DONE

--- Sneaker/js/script.js
+++ Sneaker/js/script.js
@@ -1,1 +1,8 @@
 // New Project / Start at 2025/02/18
+
+// # 顯示與隱藏 .search-box
+const search =document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
