6c12fb3748964659ec8b80657e161769d0b385a5
Norka <xieyr1093@gmail.com>
2025-02-24 20:56:49 +0800

    update

---
+++ music-player/js/music-list.js
@@ -0,0 +1,28 @@
+const allMusic = [
+    {
+        name: "DO U",
+        artist: "Kiara",
+        img: "./asset/img/kria.gif",
+        src: "./asset/music/DO-U.mp3",
+    },
+    {
+        name: "Mumei",
+        artist: "Mumei",
+        img: "./asset/img/mumei.gif",
+        src: "./asset/music/mumei.mp3",
+    },
+    {
+        name: "ChikuTaku",
+        artist: "Ame",
+        img: "./asset/img/ame.gif",
+        src: "./asset/music/ChikuTaku.mp3",
+    },
+    {
+        name: "Daydream",
+        artist: "Kronii",
+        img: "./asset/img/konii.gif",
+        src: "./asset/music/Daydream.m4a",
+    }
+]
+// 29:31
+
--- music-player/js/script.js
+++ music-player/js/script.js
@@ -1,31 +1,3 @@
-const allMusic = [
-    {
-        name: "DO U",
-        artist: "Kiara",
-        img: "./asset/img/kria.gif",
-        src: "./asset/music/DO-U.mp3",
-    },
-    {
-        name: "Mumei",
-        artist: "Mumei",
-        img: "./asset/img/mumei.gif",
-        src: "./asset/music/mumei.mp3",
-    },
-    {
-        name: "ChikuTaku",
-        artist: "Ame",
-        img: "./asset/img/ame.gif",
-        src: "./asset/music/ChikuTaku.mp3",
-    },
-    {
-        name: "Daydream",
-        artist: "Kronii",
-        img: "./asset/img/konii.gif",
-        src: "./asset/music/Daydream.m4a",
-    }
-]
-// 29:31
-
 // const playpauseBtn = document.getElementById('playpause');
 // const prevBtn = document.getElementById('prev');
 // const nextBtn = document.getElementById('next');
