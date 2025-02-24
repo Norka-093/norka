a74a7e9a90e1fb98e59ce9622128fe111624b62d
Norka <xieyr1093@gmail.com>
2025-02-24 11:11:14 +0800

    新增滾動顯示的熱門食物項目功能，包含樣式調整及滑動控制

--- new project/js/script.js
+++ new project/js/script.js
@@ -1,1 +1,21 @@
-// project start at 2025/02/23
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 3,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
