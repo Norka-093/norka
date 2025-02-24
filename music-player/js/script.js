1192b734664895f44919da45b1d0007de85420f8
Norka <xieyr1093@gmail.com>
2025-02-24 11:39:46 +0800

    test

---
+++ music-player/js/script.js
@@ -0,0 +1,20 @@
+// const playpauseBtn = document.getElementById('playpause');
+// const prevBtn = document.getElementById('prev');
+// const nextBtn = document.getElementById('next');
+// const audio = document.getElementById('audio');
+
+// playpauseBtn.addEventListener('click', function () {
+//   if (audio.paused) {
+//     audio.play();
+//     playpauseBtn.textContent = '暂停';
+//   } else {
+//     audio.pause();
+//     playpauseBtn.textContent = '播放';
+//   }
+// });
+
+// prevBtn.addEventListener('click', function () {
+// });
+
+// nextBtn.addEventListener('click', function () {
+// });
