4dd2773d3b9f42877054842d2bc2f1a1a563be74
Norka <xieyr1093@gmail.com>
2025-02-26 13:17:13 +0800

    新增兩個音樂播放清單，更新HTML結構以支援新清單，並調整CSS樣式以改善顯示效果

---
+++ music-player/js/music-list-2.js
@@ -0,0 +1,20 @@
+const allMusic = [
+    {
+        name: "Catwalk",
+        artist: "鳥屎&XICO ft.阿樂",
+        img: "catwalk.jpg",
+        src: "Catwalk.m4a",
+    },
+    {
+        name: "那年·少年",
+        artist: "宋宇寧",
+        img: "宋宇寧.jpg",
+        src: "那年·少年.m4a",
+    },
+    {
+        name: "忘川彼岸",
+        artist: "零一九零貳",
+        img: "忘川彼岸.jpg",
+        src: "忘川彼岸-DJ版.mp3",
+    }
+]
