e45e115b8c95da3be98d2c79ee3f94a42024858d
Norka <xieyr1093@gmail.com>
2025-02-27 16:27:57 +0800

    調整輪播效果的卡片偏移量，優化顯示邏輯以改善用戶體驗

--- music-player/js/carousel.js
+++ music-player/js/carousel.js
@@ -5,22 +5,38 @@ let next = document.querySelector('.btn-next');
 let active = 2;
 function loadShow(){
     let stt = 0;
+    wrapper[active].style.transform = `none`;
+    wrapper[active].style.zIndex = 1;
+    wrapper[active].style.filter = 'none';
+    wrapper[active].style.opacity = 1;
     for (var i = active +1; i < wrapper.length; i++) {
         stt ++;
         // card 偏移
-        wrapper[i].style.transform = `translateX(${10*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+        wrapper[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
         wrapper[i].style.zIndex = -stt;
-        // wrapper[i].style.filter = 'blur(5px)';
-        // wrapper[i].style.opacity = stt > 2 ? 0 : 0.6;
+        // wrapper[i].style.filter = 'blur(1px)';
+        wrapper[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
     stt = 0;
     for (var i = active - 1; i >= 0; i--) {
         stt ++;
         // card 偏移
-        wrapper[i].style.transform = `translateX(${-10*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+        wrapper[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
         wrapper[i].style.zIndex = -stt;
-        // wrapper[i].style.filter = 'blur(5px)';
-        // wrapper[i].style.opacity = stt > 2 ? 0 : 0.6;
+        // wrapper[i].style.filter = 'blur(1px)';
+        wrapper[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
+    // wrapper[0].style.transform = `translateX(${25*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+    // wrapper[4].style.transform = `translateX(${-25*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+
 }
 loadShow();
+
+next.onclick = function(){
+    active = active + 1 < wrapper.length ? active + 1 : active;
+    loadShow();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : active;
+    loadShow();
+}
